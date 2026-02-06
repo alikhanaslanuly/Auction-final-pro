6d32aa0df0fd8a892ab2068ce3ec02a98148513b
alikhanaslanuly <aslanovichalish@gmail.com>
2026-02-06 23:09:52 +0500

    Refactor deployAUC.js to deploy AuctionToken and Auction

--- scripts/deployAUC.js
+++ scripts/deployAUC.js
@@ -4,17 +4,30 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  const tokenAddress = process.env.TOKEN_ADDRESS;
-  if (!tokenAddress) throw new Error("TOKEN_ADDRESS is missing in .env");
-
   const [deployer] = await ethers.getSigners();
   console.log("Deployer:", deployer.address);
-  console.log("Token:", tokenAddress);
+
+  const BID_REWARD_PERCENT = 5; // 5% от ставки
+  const WINNER_BONUS = ethers.parseUnits("50", 18); // 50 токенов (если decimals=18)
+
+  const AuctionToken = await ethers.getContractFactory("AuctionToken");
+  const token = await AuctionToken.deploy(deployer.address);
+  await token.waitForDeployment();
+  console.log("AuctionToken:", token.target);
 
   const Auction = await ethers.getContractFactory("Auction");
-  const auction = await Auction.deploy(tokenAddress);
+  const auction = await Auction.deploy(token.target, BID_REWARD_PERCENT, WINNER_BONUS);
   await auction.waitForDeployment();
   console.log("Auction:", auction.target);
+
+  const tx = await token.setMinter(auction.target, true);
+  await tx.wait();
+  console.log("setMinter(Auction, true) done:", tx.hash);
+
+  console.log("TOKEN_ADDRESS =", token.target);
+  console.log("AUCTION_ADDRESS =", auction.target);
+  console.log("BID_REWARD_PERCENT =", BID_REWARD_PERCENT);
+  console.log("WINNER_BONUS =", WINNER_BONUS.toString());
 }
 
 main().catch((e) => {
